ddd616630242b0995eb439d2b739afaf0b5c7cdd
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-10-25 12:11:26 +0200

    TDD started

--- MovieReview/MovieReview.cs
+++ MovieReview/MovieReview.cs
@@ -1,7 +1,85 @@
+using System;
+using System.Collections.Generic;
+
 namespace MovieReview
 {
-    public class MovieReview
+    public class MovieReview : IMovieReview
     {
-        
+        public List<Review> reviews = new List<Review>();
+        public int ReviewsFromReviewer(int n)
+        {
+            int numberCount = 0;
+            foreach (var review in reviews)
+            {
+                if (review.Reviewer == n)
+                {
+                    numberCount++;
+                }
+            }
+
+            return numberCount;
+        }
+
+        public double AverageReviewerRating(int n)
+        {
+            double sum = 0;
+            double count = 0;
+            foreach (var rev in reviews)
+            {
+                if (rev.Reviewer == n)
+                {
+                    sum += rev.Grade;
+                    count++;
+                }
+            }
+
+            double avg = sum / count;
+            return avg;
+        }
+
+        public int TimesReviewerHasGivenRating(int n, int g)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TimesMovieReviewed(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double AverageMovieRating(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TimesMovieGivenGrade(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] MoviesGivenHighestRating()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] MostReviewsReviewer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] TopNMovies(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] MoviesReviewedByReviewer(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] ReviewersReviewedMovieDecreasing(int n)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ MovieReviewTest/Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using MovieReview;
+using Xunit;
+
+namespace MovieReviewTest
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void TestReviewsFromReviewer(int n)
+        {
+            MovieReview.MovieReview m = new MovieReview.MovieReview();
+            for (int i = 0; i < n; i++)
+            {
+                m.reviews.Add(new Review()
+                {
+                    Reviewer = 1,
+                    Grade = n,
+                    Movie = n,
+                    Date = DateTime.Now
+                });
+            }
+
+            Assert.True(m.ReviewsFromReviewer(1) == n);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1.5)]
+        [InlineData(3, 2)]
+        [InlineData(4, 2.5)]
+        [InlineData(5, 3)]
+        [InlineData(6, 3.5)]
+        [InlineData(7, 4)]
+        [InlineData(8, 4.5)]
+        [InlineData(9, 5)]
+        public void TestAverageReviewerRating(int n, double d)
+        {
+            MovieReview.MovieReview m = new MovieReview.MovieReview();
+            for (int i = 0; i < n; i++)
+            {
+                m.reviews.Add(new Review()
+                {
+                    Reviewer = 1,
+                    Grade = i + 1,
+                    Movie = n,
+                    Date = DateTime.Now
+                });
+            }
+
+            Assert.True(m.AverageReviewerRating(1) == d);
+        }
+
+        [Fact]
+        public void TestAverageReviewerRating2()
+        {
+            MovieReview.MovieReview m = new MovieReview.MovieReview();
+            for (int i = 0; i < 4; i++)
+            {
+                m.reviews.Add(new Review()
+                {
+                    Reviewer = 1,
+                    Grade = i + 1,
+                    Movie = 1,
+                    Date = DateTime.Now
+                });
+            }
+
+            Assert.True(m.AverageReviewerRating(1) == 2.5);
+        }
+    }
+}
